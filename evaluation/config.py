0676a3c6c2c77910c0c86aa22485061d3fde932d
tjshy <the_shy@tongji.edu.cn>
2024-04-12 17:23:10 +0800

    20240412

---
+++ evaluation/config.py
@@ -0,0 +1,49 @@
+result_path='/home/DGDRL/NDX/'
+class Config:
+    def __init__(self) -> None:
+        '''
+        1. 数据源部分
+        '''
+        self.dataset = 'NDX100'
+        self.npz_pwd = '/home/sunhaoyu/DGDRL/data2023/'+self.dataset+'/'+self.dataset+'.numpy.npz'
+        self.csv_pwd = '/home/sunhaoyu/DGDRL/data2023/'+self.dataset+'/'+self.dataset+'.csv'
+        self.relation_pwd = '/home/sunhaoyu/DGDRL/data2023/'+self.dataset+'/'+'relation2.pkl'
+        self.featureList = ['close','open','high','low','volume']
+        '''
+        2. 训练与测试
+        '''
+        self.gpu_id = 1
+        self.random_seed=40
+        self.thread_num = 16
+        self.window=20
+        self.epoch=800
+        '''
+        3. 网络参数
+        '''
+        
+        self.num_stocks = 96
+        self.input_dim=len(self.featureList)
+        self.hidden_dim = 64
+        self.output_dim = 64
+        self.dropout = 0.3
+        self.num_heads=16
+        self.negative_slope=0.2
+        self.gamma = 0.99 
+        self.initial_amount=1e6 
+        self.batch_size = 128
+        self.repeat_times = 4
+        self.reward_scale = 1
+        self.learning_rate_cri = 0.08
+        self.learning_rate_act = 0.08
+        self.ratio_clip = 0.25
+        self.lambda_entropy = 0.02
+        self.train_beg_idx = '2020-01-02'
+        self.train_end_idx = '2022-12-01'
+        self.test_beg_idx = '2022-12-01'
+        self.test_end_idx='2023-12-29'   
+        self.state_input_dim = 64 
+        self.action_dim = self.num_stocks
+        self.act_mid_layer_num=2
+        self.act_mid_dim = 64
+        self.cri_mid_layer_num=2
+        self.cri_mid_dim = 64
